d88467de26d5c560fae69f772c46482d84030b51
MetaTPT <141391928+MetaTPT@users.noreply.github.com>
2023-08-03 23:16:13 -0700

    Test

--- src/javademo/DemoGitHub.java
+++ src/javademo/DemoGitHub.java
@@ -2,14 +2,19 @@ package javademo;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class DemoGitHub {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void techlearn() {
+	  driver.get("https://www.techlearn.in");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
   }
 
   @AfterTest
